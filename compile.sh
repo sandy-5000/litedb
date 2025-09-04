e0af959122eba58ef6bac2cd599e8d06f8e31db2
Sandeep Kumar Bhaviri <sandeepkumarbhaviri@gmail.com>
2025-09-04 19:56:22 +0530

    changed executable name

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-g++ -std=c++20 -Iinclude src/main.cpp src/thread_test.cpp -o mongolite
+g++ -std=c++20 -Iinclude src/main.cpp src/thread_test.cpp -o mongolite.out
 
 
 if [ $? -eq 0 ]; then
-    echo "Compilation successful. Executable is 'mongolite'."
+    echo "Compilation successful. Executable is 'mongolite.out'."
 else
     echo "Compilation failed."
 fi
