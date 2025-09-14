cf1e6b9ddda49bcfce685633ef5a2a952ef88288
Sandeep Kumar Bhaviri <sandeepkumarbhaviri@gmail.com>
2025-09-14 20:07:27 +0530

    changed variable

--- src/page/page.cpp
+++ src/page/page.cpp
@@ -6,7 +6,7 @@ namespace litedb::page {
 Page::Page(std::uint32_t id) : id_(id), PageHeader(data_, &mtx_) {
     std::unique_lock lock(mtx_);
     std::memset(data_, 0, litedb::constants::PAGE_SIZE);
-    writeValue(0, id);
+    writeValue(PAGE_ID_OFFSET, id);
 }
 
 
