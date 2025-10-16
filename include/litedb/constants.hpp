b80fcce624daee1c08b3e112337728e2af084b4f
Sandeep Kumar Bhaviri <sandeepkumarbhaviri@gmail.com>
2025-10-16 23:14:26 +0530

    adjusted spacings

--- include/litedb/constants.hpp
+++ include/litedb/constants.hpp
@@ -4,9 +4,9 @@
 
 namespace litedb::constants {
 
-inline constexpr std::uint32_t DB_PAGE_SIZE           = 4096;
-inline constexpr std::uint32_t PAGE_HEADER_SIZE    =   64;
-inline constexpr std::uint32_t BUFFER_MANAGER_SIZE = 1331;
-inline constexpr std::uint32_t INVALID_PAGE_ID     = 0xFFFFFFFF;
+inline constexpr std::uint32_t DB_PAGE_SIZE        =       4096;
+inline constexpr std::uint32_t PAGE_HEADER_SIZE    =         64;
+inline constexpr std::uint32_t BUFFER_MANAGER_SIZE =       1331;
+inline constexpr std::uint32_t INVALID_PAGE_ID     = 0xffffffff;
 
 }
